5cda7e4f147ae58f1177b9252aab3bf2fe196628
Grace Doyle <graceydoyle@gmail.com>
2022-07-16 01:09:44 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavoritesIcon from '../components/cardIcons/addToFavourites' //added for part 4, exercise 1
 
 const UpcomingMoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
   const favourites = movies.filter((m) => m.favourite);
   localStorage.setItem("favourites", JSON.stringify(favourites));
 
-  const addToFavourites = () => null;
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
 
   useEffect(() => {
     getUpcomingMovies().then((movies) => {
@@ -21,6 +27,11 @@ const UpcomingMoviesPage = (props) => {
       title="Upcoming Movies"
       movies={movies}
       selectFavourite={addToFavourites}
+    // added as part of part 4, exercise 1
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+              }}
+              // end of addition
     />
   );
 };
