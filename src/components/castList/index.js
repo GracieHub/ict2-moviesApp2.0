94c7f3b7f3d9d28cf091ddf25b7641b5a3838b81
Grace Doyle <graceydoyle@gmail.com>
2022-08-22 01:51:06 +0100

    FixedModal by combining CastCard & CastList to one component & React Query only on MovieDetailPage now.

--- src/components/castList/index.js
+++ src/components/castList/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import CastMemberCard from "../castCard";
-import Grid from "@material-ui/core/Grid";
-import { useParams } from "react-router-dom";
-import { getCast } from "../../api/tmdb-api";
-import { useQuery } from "react-query";
-import Spinner from "../spinner";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardHeader from "@material-ui/core/CardHeader";
+import Typography from "@material-ui/core/Typography";
 import AddIcon from "@material-ui/icons/Add";
-import { getPersonDetail } from "../../api/tmdb-api";
-import CastModal from "../castModal";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles({
     root: {
@@ -31,69 +30,63 @@ const useStyles = makeStyles({
     plusIcon: {
       cursor: "pointer",
     },
+    card: { maxWidth: 300 },
+    media: { height: 250 },
+
+    title: {
+      color: "#FFF",
+    },
+    titleBar: {
+      background:
+        "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+    },
+    castTitle: {
+      margin: "20px 0",
+    },
   });
 
   
-
-const CastList = ( cast) => {
-  const { id } = useParams();
+export default function CastList ({ actor, handleClickOpen }) {
   const classes = useStyles();
   
-  const [open, setOpen] = React.useState(false);
-  const [actorDetail, setActorDetails] = React.useState({});
-
-  const handleClickOpen = async (id) => {
-    console.log(id);
-
-    const personDetail = await getPersonDetail(id);
-
-    setOpen(true);
-    setActorDetails({
-      name: personDetail.name,
-      from: personDetail.place_of_birth,
-      popularity: personDetail.popularity,
-      biography: personDetail.biography,
-      dateOfBirth: personDetail.birthday,
-      image: personDetail.profile_path,
-    });
-  };
-  const handleClose = () => {
-    setOpen(false);
-    setActorDetails({});
-  };
-  
-  const {  data, error, isLoading, isError }  = useQuery(['cast', {id: id}], getCast)
-  
-  if (isLoading) {
-    return <Spinner />
-  }
-
-  if (isError) {
-    return <h1>{error.message}</h1>
-  }  
-  const credits = data;
-  console.log(data);
-
-  let castMemberCards = credits.cast.map((a) => (
-    <Grid key={a.id} item xs={6} sm={5} md={4} lg={3} xl={2}>
-        <CastMemberCard key={a.id} actor={a} 
-        handleClickOpen={handleClickOpen} />
-        <CastModal
-        handleClose={handleClose}
-        actorDetail={actorDetail}
-        open={open}/>
-        <AddIcon
-              className={classes.plusIcon}
-              onClick={() => handleClickOpen(a.id)}
+  return (
+  <>
+    {actor.cast.map((a) => (
+    <Grid className= {classes.root} key={a.id} item xs={6} sm={5} md={4} lg={3} xl={2}>
+        <Card className={classes.card}
+              key={a.id} 
+              actor={a} 
+              handleClickOpen={handleClickOpen}>
+            <CardHeader
+                className={classes.header}
+                title={
+                    <Typography variant="h5" component="p">
+                        {a.name}{" "}
+                    </Typography>
+                }
             />
-            <CastModal
-              handleClose={handleClose}
-              actorDetail={actorDetail}
-              open={open}
+            <CardMedia
+                className={classes.media}
+                image={
+                    a.profile_path
+                        ? `https://image.tmdb.org/t/p/w500/${a.profile_path}`
+                        : `${process.env.PUBLIC_URL}/assets/actor.jpg`
+                }
             />
+            <CardContent>
+                <Typography variant="h6" component="p">
+                    as {a.character}
+                </Typography>
+                <AddIcon
+                  className={classes.plusIcon}
+                  onClick={() => handleClickOpen(a.id)}>
+                </AddIcon>
+            </CardContent>
+            <CardActions disableSpacing>
+            </CardActions>
+        </Card>
     </Grid>
-  ));
-  return castMemberCards;
-};
-
-export default CastList;
+    ))}
+  </>
+    );
+  }
--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,50 +1,15 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import MovieDetails from "../components/movieDetails";
 import PageTemplate from "../components/templateMoviePage";
-// import useMovie from "../hooks/useMovie";
 import { useQuery } from "react-query";
 import Spinner from '../components/spinner'
 import { getMovie, getSimilarMovies, getCast } from "../api/tmdb-api";
 import SimilarMovies from "../components/similarMovies";
 import CastList from "../components/castList";
-import Paper from "@material-ui/core/Paper";
-import { Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import { getPersonDetail } from "../api/tmdb-api";
 import CastModal from "../components/castModal";
-
-
-
-const useStyles = makeStyles((theme) => ({
-  chipRoot: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    flexWrap: "wrap",
-    listStyle: "none",
-    padding: theme.spacing(1.5),
-    margin: 0,
-  },
-  chipSet: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexWrap: "wrap",
-    listStyle: "none",
-    padding: theme.spacing(1.5),
-    margin: 0,
-  },
-  chipLabel: {
-    margin: theme.spacing(0.5),
-  },
-  fab: {  //New
-    position: "fixed",
-    top: theme.spacing(15),
-    right: theme.spacing(2),
-  },
-}));
+import { useParams } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
 const MovieDetailsPage = (props) => {
 
@@ -53,11 +18,12 @@ const MovieDetailsPage = (props) => {
 
   const handleClickOpen = async (id) => {
     console.log(id);
-
     const personDetail = await getPersonDetail(id);
 
     setOpen(true);
+
     setActorDetails({
+      id: personDetail.id,
       name: personDetail.name,
       from: personDetail.place_of_birth,
       popularity: personDetail.popularity,
@@ -66,25 +32,16 @@ const MovieDetailsPage = (props) => {
       image: personDetail.profile_path,
     });
   };
+  
   const handleClose = () => {
     setOpen(false);
     setActorDetails({});
   };
 
   const { id } = useParams();
-  const classes = useStyles();
-  const { data: movie, error, isLoading, isError } = useQuery(
-    ["movie", { id: id }],
-    getMovie
-  );
-
-  const { data: similarMovies } = useQuery(
-    ["similarMovies", { id: id }],
-    getSimilarMovies
-  );
-
+  const { data: movie, error, isLoading, isError } = useQuery(["movie", { id: id }],getMovie);
+  const { data: similarMovies } = useQuery(["similarMovies", { id: id }],getSimilarMovies);
   const { data: castList } = useQuery(["castList", { id: id }], getCast);
-
 
   if (isLoading) {
     return <Spinner />;
@@ -99,23 +56,24 @@ const MovieDetailsPage = (props) => {
       {movie && similarMovies && castList? (
         <>
           <PageTemplate movie={movie}>
-            <SimilarMovies itemData={similarMovies} />
             <MovieDetails movie={movie} />
-            <Paper component="ul" className={classes.chipSet}>
-          <h2>Movie Cast</h2>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            <CastList cast={castList} handleClickOpen={handleClickOpen} />
+            <SimilarMovies itemData={similarMovies} />
+            <h2>Movie Cast</h2>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <CastList 
+              actor={castList} 
+              handleClickOpen={handleClickOpen} />
             <CastModal
               handleClose={handleClose}
               actorDetail={actorDetail}
               open={open}
             />
-          </Grid>
-        </Paper>
+            </Grid>
+
           </PageTemplate>
         </>
       ) : (
-        <p>Waiting for movie details</p>
+        <Spinner />
       )}
     </>
   );
